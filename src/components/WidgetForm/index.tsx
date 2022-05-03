5014b79a9a2820914f76f778a98f204320eb65bd
Gustavo Teodoro Bauke <baukegustavo@gmail.com>
2022-05-03 14:05:39 -0300

    Aula 02

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+import { useState } from 'react';
+
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackType } from './feedbackTypes';
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  const handleRestartFeedback = () => {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  };
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequest={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequest={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ❤ por{' '}
+        <a
+          className="underline underline-offset-2"
+          href="https://github.com/gtbauke"
+        >
+          Gustavo Bauke
+        </a>
+      </footer>
+    </div>
+  );
+}
